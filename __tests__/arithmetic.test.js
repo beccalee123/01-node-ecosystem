ab6da8f9625d65430cb84e70d95da40db583899e
BECCA LEE <REBECCA.KIRSTIN.LEE@GMAIL.COM>
2018-12-24 11:03:45 -0800

    Adds multiplicationa nd division

--- __tests__/arithmetic.test.js
+++ __tests__/arithmetic.test.js
@@ -50,5 +50,56 @@ describe('Subtraction', () => {
   it('does not accept strings as params', () => {
     let vals = arithmetic.subtract('hi', 'how are you');
     expect(vals).toBeNull();
-  })
+  });
+});
+
+describe('Multiplication', () => {
+  it('works when two inputs are provided', () => {
+    let vals = arithmetic.multiply(numOne, numTwo);
+    let expectedOutput = numOne * numTwo;
+    expect(vals).toEqual(expectedOutput);
+  }); 
+
+  it('requires more than one param', () => {
+    let vals = arithmetic.multiply(numOne);
+    expect(vals).toBeNull();
+  });
+
+  it('needs more than zero params', () => {
+    let vals = arithmetic.multiply();
+    expect(vals).toBeNull();
+  });
+
+  it('does not accept strings as params', () => {
+    let vals = arithmetic.multiply('wow', 'a string!');
+    expect(vals).toBeNull();
+  });
+});
+
+describe('Division', () => {
+  it('works when two inputs are provided', () => {
+    let vals = arithmetic.divide(numOne, numTwo);
+    let expectedOutput = numOne / numTwo;
+    expect(vals).toEqual(expectedOutput);
+  }); 
+
+  it('requires more than one param', () => {
+    let vals = arithmetic.divide(numOne);
+    expect(vals).toBeNull();
+  });
+
+  it('needs more than zero params', () => {
+    let vals = arithmetic.divide();
+    expect(vals).toBeNull();
+  });
+
+  it('does not accept strings as params', () => {
+    let vals = arithmetic.divide('wow', 'a string!');
+    expect(vals).toBeNull();
+  });
+
+  it('does not accept 0 as a param', () => {
+    let vals = arithmetic.divide(numOne, 0);
+    expect(vals).toBeNull();
+  });
 });
